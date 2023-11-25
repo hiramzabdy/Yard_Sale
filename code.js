1c6d21a6a4894cbc2ad9ce607ca8c5809ed2f187
Hiram Zabdy <dsbzwh@gmail.com>
2023-11-24 20:42:59 -0600

    fused with shopping cart

--- code.js
+++ code.js
@@ -1,9 +1,34 @@
-const navbarEmail = document.querySelector(".navbar-email")
+const desktopClickMenu = document.querySelector(".navbar-email")
+const mobileClickMenu = document.querySelector(".menu")
 const desktopMenu = document.querySelector(".desktop-menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+desktopClickMenu.addEventListener("click", toggleMenuDesktop)
+mobileClickMenu.addEventListener("click", toggleMenuMobile)
 
-navbarEmail.addEventListener("click", toggleMenuDesktop)
+const shoppingCartIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
+shoppingCartIcon.addEventListener("click", toggleAside)
 
 function toggleMenuDesktop(){
-    console.log("Click")
-    desktopMenu.classList.toggle("inactive")
+    if(!aside.classList.contains("inactive") || !desktopMenu.classList.contains("inactive")){
+        aside.classList.add("inactive")
+    }else{
+        desktopMenu.classList.toggle("inactive")
+    }
+}
+function toggleMenuMobile(){
+    if(aside.classList.contains("inactive")){
+        mobileMenu.classList.toggle("inactive")
+    }else{
+        toggleAside()
+        mobileMenu.classList.toggle("inactive")
+    }
+}
+function toggleAside(){
+    if(mobileMenu.classList.contains("inactive")){
+        aside.classList.toggle("inactive")
+    }else{
+        toggleMenuMobile()
+        aside.classList.toggle("inactive")
+    }
 }
